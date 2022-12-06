c394c0239c9f6110d22571c41d7b0ef146fa96ac
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-12-06 09:34:37 -0800

    Lesson 7 completed: controls, orbit control

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,5 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -15,8 +17,8 @@ scene.add(mesh)
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600,
 }
 
 // Camera
@@ -24,9 +26,33 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+// Enable damping
+controls.enableDamping = true
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Animations
+// gsap.to(mesh.position, { x: 2, duration: 1, delay: 1 })
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+  //   const currentTime = Date.now()
+  //   const deltaTime = currentTime - time
+  //   time = currentTime
+
+  //   mesh.rotation.x += 0.001 * deltaTime
+  //   mesh.rotation.y += 0.001 * deltaTime
+  controls.update()
+  const elapsedTime = clock.getElapsedTime()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+
+window.requestAnimationFrame(tick)
